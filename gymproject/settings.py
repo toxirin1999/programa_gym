1cea2ce22cc8e9e387fb390af1c88683162f28e9
toxirin1999 <davidmillanblanco@gmail.com>
2025-06-28 10:59:54 +0200

    Update settings.py

--- gymproject/settings.py
+++ gymproject/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'clientes.apps.ClientesConfig'
+    'clientes.apps.ClientesConfig',
     'rutinas',
     'dietas',
     'anuncios',
