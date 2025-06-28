cfda3cb09371cd2057431cca3087ae7170db7c2a
toxirin1999 <davidmillanblanco@gmail.com>
2025-06-28 09:09:33 +0200

    Update apps.py

--- clientes/apps.py
+++ clientes/apps.py
@@ -1,6 +1,4 @@
-from django.apps import AppConfig
-
-
 class ClientesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'clientes'
+    label = 'clientes_app'  # 🔧 Esto evita el conflicto
