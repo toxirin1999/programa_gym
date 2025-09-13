f8f3b89dbe073669f419a728fd6c4377da2f8964
toxirin1999 <davidmillanblanco@gmail.com>
2025-09-13 12:26:11 +0200

    FIX: Añadir la raíz del proyecto al PYTHONPATH para el despliegue

--- gymproject/asgi.py
+++ gymproject/asgi.py
@@ -1,15 +1,12 @@
-"""
-ASGI config for gymproject project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
+# gymproject/asgi.py
 import os
-
+import sys
 from django.core.asgi import get_asgi_application
+
+# --- AÑADE ESTAS LÍNEAS ---
+# Añade la carpeta raíz del proyecto al PYTHONPATH
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+# -------------------------
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gymproject.settings')
 
