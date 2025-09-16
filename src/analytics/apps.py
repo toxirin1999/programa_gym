92a7010784a4f4d99f469fab86ae9c3f4905ce7c
toxirin1999 <davidmillanblanco@gmail.com>
2025-09-16 17:07:19 +0200

    Update apps.py

--- src/analytics/apps.py
+++ src/analytics/apps.py
@@ -1,10 +1,15 @@
+# src/analytics/apps.py
+import os # <-- AÑADE ESTA LÍNEA
 from django.apps import AppConfig
 
 class AnalyticsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'analytics'
-    verbose_name = 'Centro de Análisis'
-    
+
     def ready(self):
-        # Importar señales si las hay
+        # Si la variable de entorno está presente, NO importes los signals.
+        if os.environ.get('DJANGO_IMPORTING_DATA') == 'True':
+            return
+
+        # Si no, importa los signals como siempre.
         import analytics.signals
